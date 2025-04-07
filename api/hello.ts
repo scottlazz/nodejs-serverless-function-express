4dbe0cb12a82565d3962d73176052d418e2ff88b
Scott <dikowitz@gmail.com>
2025-04-07 19:46:57 -0400

    Update hello.ts

--- api/hello.ts
+++ api/hello.ts
@@ -1,8 +1,9 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query
-  return res.json({
-    message: `Hello ${name}!`,
-  })
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const { symbol = 'GME' } = req.query
+  const period = Math.floor(Date.now() / 1000);
+  const data = await fetch(`https://query2.finance.yahoo.com/v8/finance/chart/${symbol}?period1=${period}&period2=${period}&interval=1m&includePrePost=true`);
+  const json = await data.json();
+  return res.json(json);
 }
